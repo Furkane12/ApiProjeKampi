051efd2ee7be83070cbe55bdb3408b671efc6261
Furkan Eroğlu <furkaneroglu134@gmail.com>
2025-12-24 15:50:22 +0300

    Entities`ler eklendi.

---
+++ ApiProjeKampi.WebApi/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
