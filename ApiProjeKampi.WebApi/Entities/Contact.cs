1d116089bba53c6d90b5f44afd027a1a908c35b0
Furkan Eroğlu <furkaneroglu134@gmail.com>
2025-12-24 16:05:07 +0300

    paketler ve eksik olan entitiesler kuruldu.

--- ApiProjeKampi.WebApi/Entities/Contact.cs
+++ ApiProjeKampi.WebApi/Entities/Contact.cs
@@ -3,9 +3,10 @@
     public class Contact
     {
         public int ContactId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string MapLocation { get; set; }
+        public string Adress { get; set; }
+        public string Phone { get; set; }
         public string Email { get; set; }
-        public string Message { get; set; }
+        public string OpenHours { get; set; }
     }
 }
