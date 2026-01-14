d8d58e354eeb06250d27731d05a564eb950a60b9
Furkan Eroğlu <furkaeroglu131@gmail.com>
2026-01-14 21:24:07 +0300

    Entitylerdeki hatalar düzeltildi.

---
+++ ApiProjeKampi.WebApi/Migrations/20260114181740_FixTables.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiProjeKampi.WebApi.Migrations
+{
+    public partial class FixTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProducyId",
+                table: "Products",
+                newName: "ProductId");
+
+            migrationBuilder.RenameColumn(
+                name: "Emaail",
+                table: "Messages",
+                newName: "Email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Products",
+                newName: "ProducyId");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Messages",
+                newName: "Emaail");
+        }
+    }
+}
